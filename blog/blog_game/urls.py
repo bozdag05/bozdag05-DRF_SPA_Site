8469fd81209ebe5cf23825fa9dae815dce59826a
Boz_Dag <BozDag@umma.tu>
2024-02-06 14:54:54 +0300

    create login form and logout

--- blog/blog_game/urls.py
+++ blog/blog_game/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomeView, PostView, register
+from .views import HomeView, PostView, register, user_login, user_logout
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
     path('<slug:slug>/', PostView.as_view(), name='post_detail'),
 ]
